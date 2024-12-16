5feb448773d55652716464cc110ef1b1f9bb6661
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-16 18:11:10 +0600

    59-8 Save job application and get data using email

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,68 +1,107 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import useAuth from '../../custom-hooks/UseAuth';
+import React from "react";
+import { useParams } from "react-router-dom";
+import useAuth from "../../custom-hooks/UseAuth";
+import Swal from "sweetalert2";
 
 export default function JobApply() {
-    const {id} = useParams();
-    const {user} = useAuth();
-    // console.log(id, user);
+  const { id } = useParams();
+  const { user } = useAuth();
+  // console.log(id, user);
 
+  const handleSubmitApplication = (e) => {
+    e.preventDefault();
 
-    const handleSubmitApplication = e =>{
-        e.preventDefault();
+    const form = e.target;
+    const linkedIn = form.linkedIn.value;
+    const github = form.github.value;
+    const resume = form.resume.value;
+    // console.log(linkedIn, github, resume);
 
-        const form = e.target;
-        const linkedIn = form.linkedIn.value;
-        const github = form.github.value;
-        const resume = form.resume.value;
-        // console.log(linkedIn, github, resume);
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedIn,
+      github,
+      resume,
+    };
+    console.log(jobApplication);
 
-        const jobApplication = {
-            job_id: id,
-            applicant_email: user.email,
-            linkedIn,
-            github,
-            resume
+    fetch("http://localhost:5000/job-applications", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(jobApplication),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Successfully Applied",
+            showConfirmButton: false,
+            timer: 1500,
+          });
         }
-        console.log(jobApplication);
-    }
+      });
+  };
 
   return (
     <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Apply now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Apply now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleSubmitApplication} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">LinkedIn</span>
+              </label>
+              <input
+                type="url"
+                name="linkedIn"
+                placeholder="LinkedIn URL"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Github URL</span>
+              </label>
+              <input
+                type="url"
+                name="github"
+                placeholder="github URL"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Resume</span>
+              </label>
+              <input
+                type="url"
+                name="resume"
+                placeholder="Resume URL"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Apply</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form onSubmit={handleSubmitApplication} className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">LinkedIn</span>
-          </label>
-          <input type="url" name='linkedIn' placeholder="LinkedIn URL" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Github URL</span>
-          </label>
-          <input type="url" name='github' placeholder="github URL" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Resume</span>
-          </label>
-          <input type="url" name='resume' placeholder="Resume URL" className="input input-bordered" required />
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Apply</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-  )
+  );
 }
