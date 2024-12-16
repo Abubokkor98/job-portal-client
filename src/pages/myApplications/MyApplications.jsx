8837b732865e1570d0a9675bd4a5b04f7c7e99d1
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-16 22:02:05 +0600

    show my applications in table

--- src/pages/myApplications/MyApplications.jsx
+++ src/pages/myApplications/MyApplications.jsx
@@ -1,18 +1,69 @@
-import React, { useEffect, useState } from 'react'
-import useAuth from '../../custom-hooks/UseAuth'
+import React, { useEffect, useState } from "react";
+import useAuth from "../../custom-hooks/UseAuth";
 
 export default function MyApplications() {
-    const {user} = useAuth();
-    const [jobs, setJobs] = useState([]);
+  const { user } = useAuth();
+  const [jobs, setJobs] = useState([]);
 
-    useEffect(()=>{
-        fetch(`http://localhost:5000/job-application?email=${user.email}`)
-        .then(res=>res.json())
-        .then(data=>setJobs(data))
-    },[user.email])
+  useEffect(() => {
+    fetch(`http://localhost:5000/job-application?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, [user.email]);
   return (
     <div>
-        <h2 className="text-3xl">MyApplications {jobs.length}</h2>
+      <h2 className="text-3xl">MyApplications {jobs.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>Company</th>
+              <th>Job Title</th>
+              <th>Favorite Color</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {jobs.map((job) => (
+              <tr key={job._id}>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img src={job.company_logo} alt={job.company} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{job.company}</div>
+                      <div className="text-sm opacity-50">{job.location}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {job.title}
+                  <br />
+                </td>
+                <td>Purple</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
