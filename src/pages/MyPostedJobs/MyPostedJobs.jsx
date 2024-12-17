9df2483724f2a53ad56db9842818c8b80f64d1b9
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-17 12:47:12 +0600

    59_5-6 Show My Posted Jobs And Count Job Applications

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -1,19 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import useAuth from '../../custom-hooks/UseAuth';
+import React, { useEffect, useState } from "react";
+import useAuth from "../../custom-hooks/UseAuth";
 
 export default function MyPostedJobs() {
-    const [jobs, setJobs] = useState([]);
-    const {user} = useAuth();
+  const [jobs, setJobs] = useState([]);
+  const { user } = useAuth();
 
-    useEffect(()=>{
-        fetch(`http://localhost:5000/jobs?email=${user.email}`)
-        .then(res=>res.json())
-        .then(data=>setJobs(data))
-    },[user.email])
+  useEffect(() => {
+    fetch(`http://localhost:5000/jobs?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, [user.email]);
 
   return (
     <div>
-        <h2>My Posted Jobs {jobs.length}</h2>
+      <h2>My Posted Jobs {jobs.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th> 
+              <th>DeadLine</th>
+              <th>Application Count</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {jobs.map((job, index) => (
+              <tr key={index}>
+                <th>{index+1}</th>
+                <td>{job.title}</td>
+                <td>{job.applicationDeadline}</td>
+                <td>{job.applicationCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
