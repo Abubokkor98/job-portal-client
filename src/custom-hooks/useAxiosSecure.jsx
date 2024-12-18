a0cec3fe425a658fe7a90952e035ecea5a754b65
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-18 21:51:59 +0600

    61-6 Auth Interceptor using axios

--- src/custom-hooks/useAxiosSecure.jsx
+++ src/custom-hooks/useAxiosSecure.jsx
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { useEffect } from "react";
+import useAuth from "./UseAuth";
+import { useNavigate } from "react-router-dom";
 
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5000",
@@ -6,5 +9,27 @@ const axiosInstance = axios.create({
 });
 
 export default function useAxiosSecure() {
+  const { signOutUser } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log("error caught in interceptror", error);
+        if (error.status === 401 || error.status === 403) {
+          console.log("need to logout this user");
+          signOutUser()
+            .then(() => {
+              console.log("user logged out from axios");
+              navigate("/signIn");
+            })
+            .catch((error) => console.log(error));
+        }
+      }
+    );
+  }, []);
+
   return axiosInstance;
 }
