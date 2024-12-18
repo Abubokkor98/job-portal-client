275c674bc813019d4f4490bd113ff4b4ca35d9c8
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-18 12:47:57 +0600

    60-2 create sample jwt token and use axios to get the token

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export default function ViewApplications() {
   const applications = useLoaderData();
+
+  const handleStatusUpdate = (e, id) => {
+    console.log(e.target.value, id);
+    const data = {
+      status: e.target.value,
+    };
+    fetch(`http://localhost:5000/job-applications/${id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Status has been updated.",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
+
   return (
     <div>
       <h2 className="text-3xl">
@@ -15,18 +43,32 @@ export default function ViewApplications() {
             <tr>
               <th></th>
               <th>Applicant Email</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
+              <th>Status</th>
+              <th>Update Status</th>
             </tr>
           </thead>
           <tbody>
             {/* row 1 */}
             {applications.map((ap, i) => (
               <tr key={ap._id}>
-                <th>{i+1}</th>
+                <th>{i + 1}</th>
                 <td>{ap.applicant_email}</td>
                 <td>Quality Control Specialist</td>
-                <td>Blue</td>
+                <td>
+                  <select
+                    onChange={(e) => handleStatusUpdate(e, ap._id)}
+                    defaultValue={ap.status || "Change Status"}
+                    className="select select-bordered select-xs w-full max-w-xs"
+                  >
+                    <option disabled selected>
+                      Change Status
+                    </option>
+                    <option>Unser Review</option>
+                    <option>Set Interview</option>
+                    <option>Hired</option>
+                    <option>Rejected</option>
+                  </select>
+                </td>
               </tr>
             ))}
           </tbody>
